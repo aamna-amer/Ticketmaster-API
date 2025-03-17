4eaa92c5fdf8cdb0b1b341cba5812e0d35c50134
aamna-amer <aamna_a@live.com>
2025-03-17 10:02:09 -0700

    Update test-data_processing.R

--- tests/testthat/test-data_processing.R
+++ tests/testthat/test-data_processing.R
@@ -1,6 +1,6 @@
 test_that("get_full_ticketmaster_data() returns a valid dataframe", {
   skip_if_not(Sys.getenv("TICKETMASTER_API_KEY") != "", "API key not set, skipping test.")
-  api_key <- get_full_ticketmaster_data()
+  api_key <- get_ticketmaster_api_key()
   print(paste("API Key in test:", api_key))
   events_df <- get_full_ticketmaster_data()
   expect_s3_class(events_df, "data.frame")  # Must return a dataframe
